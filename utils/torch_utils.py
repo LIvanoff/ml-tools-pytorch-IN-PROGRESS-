a193596143bfb7b1e8310ee9385a4a0f549d4c45
LIvanoff <drfrily@gmail.com>
2023-08-21 16:54:33 +0300

    fixed function '__select_model' I rewrote the model selection function, because in the future the pretrained variable will be removed from pytorch

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -15,69 +15,69 @@ def __select_optimizer():
     pass
 
 
-def __select_model(repo_or_dir, model_name, weights, pretrained):
+def __select_model(repo_or_dir, model_name, weights):
     if repo_or_dir == '':
-        res = re.findall(r'(\w+?)(\d+)',model_name)[0]
+        res = re.findall(r'(\w+?)(\d+)', model_name)[0]
         model_name, model_num = res[0], res[1]
         match model_name:
             case 'resnet':
                 match model_num:
                     case '18':
-                        return models.resnet18(pretrained=pretrained)
+                        return models.resnet18(weights=weights)
                     case '34':
-                        return models.resnet34(pretrained=pretrained)
+                        return models.resnet34(weights=weights)
                     case '50':
-                        return models.resnet50(pretrained=pretrained)
+                        return models.resnet50(weights=weights)
                     case '101':
-                        return models.resnet101(pretrained=pretrained)
+                        return models.resnet101(weights=weights)
                     case '152':
-                        return models.resnet152(pretrained=pretrained)
+                        return models.resnet152(weights=weights)
             case 'alexnet':
-                return models.alexnet(pretrained=pretrained)
+                return models.alexnet(weights=weights)
             case 'vgg':
                 match model_num:
                     case '11':
-                        return models.vgg11(pretrained=pretrained)
+                        return models.vgg11(weights=weights)
                     case '13':
-                        return models.vgg13(pretrained=pretrained)
+                        return models.vgg13(weights=weights)
                     case '16':
-                        return models.vgg16(pretrained=pretrained)
+                        return models.vgg16(weights=weights)
                     case '19':
-                        return models.vgg19(pretrained=pretrained)
+                        return models.vgg19(weights=weights)
             case 'googlenet':
-                return models.googlenet(pretrained=pretrained)
+                return models.googlenet(weights=weights)
             case 'densenet':
                 match model_num:
                     case '121':
-                        return models.densenet121(pretrained=pretrained)
+                        return models.densenet121(weights=weights)
                     case '161':
-                        return models.densenet161(pretrained=pretrained)
+                        return models.densenet161(weights=weights)
                     case '169':
-                        return models.densenet169(pretrained=pretrained)
+                        return models.densenet169(weights=weights)
                     case '169':
-                        return models.densenet201(pretrained=pretrained)
+                        return models.densenet201(weights=weights)
             case 'efficientnet_b':
                 match model_num:
                     case '0':
-                        return models.efficientnet_b0(pretrained=pretrained)
+                        return models.efficientnet_b0(weights=weights)
                     case '1':
-                        return models.efficientnet_b1(pretrained=pretrained)
+                        return models.efficientnet_b1(weights=weights)
                     case '2':
-                        return models.efficientnet_b2(pretrained=pretrained)
+                        return models.efficientnet_b2(weights=weights)
                     case '3':
-                        return models.efficientnet_b3(pretrained=pretrained)
+                        return models.efficientnet_b3(weights=weights)
                     case '4':
-                        return models.efficientnet_b4(pretrained=pretrained)
+                        return models.efficientnet_b4(weights=weights)
                     case '5':
-                        return models.efficientnet_b5(pretrained=pretrained)
+                        return models.efficientnet_b5(weights=weights)
                     case '6':
-                        return models.efficientnet_b6(pretrained=pretrained)
+                        return models.efficientnet_b6(weights=weights)
                     case '7':
-                        return models.efficientnet_b7(pretrained=pretrained)
+                        return models.efficientnet_b7(weights=weights)
             case 'mobilenet_v':
-                return models.mobilenet_v2(pretrained=pretrained)
+                return models.mobilenet_v2(weights=weights)
             case 'inception_v':
-                return models.inception_v3(pretrained=pretrained)
+                return models.inception_v3(weights=weights)
             case _:
                     msg = f"NameError: {model_name + model_num} is unknown model, " \
                           "to download any model from pytorch, use repo_or_dir, model_name and  weights"
@@ -87,9 +87,6 @@ def __select_model(repo_or_dir, model_name, weights, pretrained):
         if weights != '':
             if weights.endswith('.pt'):
                 weights = weights.lower().replace('.pt', '')
-            return torch.hub.load(repo_or_dir=repo_or_dir, model=model_name, pretrained=pretrained, weights=weights)
+            return torch.hub.load(repo_or_dir=repo_or_dir, model=model_name, weights=weights)
         else:
-            return torch.hub.load(repo_or_dir=repo_or_dir, model=model_name, pretrained=pretrained)
-
-
-
+            return torch.hub.load(repo_or_dir=repo_or_dir, model=model_name)
