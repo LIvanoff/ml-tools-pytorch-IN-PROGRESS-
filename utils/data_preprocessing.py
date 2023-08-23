7c87f79aee2e2b0ec023056431360b7acb82be0b
Leonid <drfrily@gmail.com>
2023-08-24 02:55:58 +0300

    update data_preprocessing.py

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -28,19 +28,21 @@ def __check_dataset(dataset_path, root, sep):
         raise FileNotFoundError(msg)
 
     if dataset_path.is_file():
-        if not '.xlsx' or '.csv' in filename:
+        if '.xlsx' or '.csv' in filename:
             if sep is None:
                 msg += "Missing 1 required positional argument: 'sep'"
                 raise TypeError(msg)
             else:
-                return dataset_path
+                filetype = pathlib.Path(dataset_path).suffix
+                return [dataset_path], filetype
         else:
             # filename_list = filename.split('.')[-1]
             # filetype = filename_list[-1]
             msg += f"Extension *.{filename.split('.')[-1]} not implemented"
             raise NotImplementedError(msg)
     else:
-        pass
+        is_table = False
+        return [dataset_path], is_table
 
     # if sep is not None:
     #     file_name = dataset_path
@@ -48,7 +50,6 @@ def __check_dataset(dataset_path, root, sep):
     #         msg += "Too many files in the datasets folder: select one of them"
     #         raise FileNotFoundError(msg)
     #     elif file_name != '':
-
 
     # for i in os.listdir(datasets_folder):
     #     print(i)
