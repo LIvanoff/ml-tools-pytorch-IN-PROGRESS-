50efa20eb5ea34cc64a58ec8b9df3ab6245f0cfe
Leonid <drfrily@gmail.com>
2023-09-13 17:34:48 +0300

    added plot labels

--- utils/plot.py
+++ utils/plot.py
@@ -2,11 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_loss(train, val, epochs):
+def plot_loss(train, val, loss_name):
     plt.clf()
     plt.plot(np.arange(len(train)), train, 'r', alpha=0.8, label='train')  # c='#7B68EE'
     if len(val) != 0:
-        plt.plot(np.arange(len(val)), val, marker='o', c='#4169E1', label='val')
+        plt.plot(np.arange(len(val)), val, c='#4169E1', label='val')
+    plt.xlabel('Epochs')
+    plt.ylabel(f'{loss_name}')
     plt.legend()
     plt.grid(alpha=0.4)
     plt.draw()
