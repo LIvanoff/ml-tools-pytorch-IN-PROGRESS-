2fe31cf4bf043dc545022c55b0712e0ac7ea48f7
Leonid <drfrily@gmail.com>
2023-10-05 17:32:45 +0300

    update utils.py, added the function of transferring data to the selected device

--- utils/utils.py
+++ utils/utils.py
@@ -8,3 +8,15 @@ def save_checkpoint(epoch, model, checkpoint_dir, best=False):
     else:
         path = os.path.join(checkpoint_dir, 'model_epoch_{}.pt'.format(epoch))
     torch.save(model.state_dict(), path)
+
+
+def to_device(device: str, *args):
+    for data in args:
+        yield data.to(device)
+
+
+# x = torch.ones(10)
+# y = torch.ones(10)
+#
+# x,y = to_device('cpu', x,y)
+# print(x.get_device())
